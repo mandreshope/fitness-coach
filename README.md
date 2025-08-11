b87c0adfc32cbcfef63884c29024e3324148a2b5
mandreshope <mandreshope@gmail.com>
2025-08-11 15:21:01 +0300

    chore: Updates app icons and configures Android signing
    Integrates `flutter_launcher_icons` to automate the generation and management of app icons for both Android and iOS, replacing previous icon assets.
    
    Configures Android release signing within `build.gradle.kts` to load credentials from a `key.properties` file, enabling proper signing for release builds. Also updates the Android application's display name.

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## flutter_launcher_icons
+
+Icon launcher generator
+
+- [Docs here](https://pub.dev/packages/flutter_launcher_icons)
+
+```
+flutter pub run flutter_launcher_icons:main
+```
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,13 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+//++config for release
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -30,11 +40,21 @@ android {
         versionName = flutter.versionName
     }
 
+    //++config for release
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
